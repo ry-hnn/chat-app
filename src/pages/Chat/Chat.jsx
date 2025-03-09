d25be835f45c586bfabcafab5ca0e6329a5c8d26
Ryan Hennessy <rhennessy.tech@outlook.com>
2025-03-09 09:15:50 -0400

    testing.1

---
+++ src/pages/Chat/Chat.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useEffect, useState } from 'react'
+import './Chat.css'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
+import { AppContext } from '../../context/AppContext'
+import Chatbox from '../../components/Chatbox/Chatbox'
+const Chat = () => {
+
+  const { chatData, userData } = useContext(AppContext);
+  const [loading, setLoading] = useState(true);
+  
+
+  useEffect(() => {
+    if (chatData && userData) {
+      setLoading(false); 
+    }
+  }, [chatData,userData])
+
+  return (
+    <div className='chat'>
+      {loading
+        ?<p className='loading'>
+          Loading...
+        </p>
+        : <div className="chat-container">
+          <LeftSidebar />
+          <Chatbox />
+          <RightSidebar />
+        </div>
+      }
+
+    </div>
+  )
+}
+
+export default Chat
